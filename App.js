50ca0079622a8d035afa5f9aa313e4a02b416b6c
Faris Widyantho <radenfaris@gmail.com>
2018-01-11 11:14:43 +0700

    human tracker

--- App.js
+++ App.js
@@ -15,54 +15,227 @@ import {
 } from 'react-native';
 import { SensorManager } from 'NativeModules';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
-
 export default class App extends Component<{}> {
   constructor() {
     super()
-    this.state = {}
+    this.state = {
+      step: 0,
+      accel: false,
+      gyro: false,
+      light: false,
+      status: 'unknown',
+      gyroX: 0.00,
+      gyroY: 0.00,
+      gyroZ: 0.00,
+      accelX: 0.00,
+      accelY: 0.00,
+      accelZ: 0.00,
+      lightSensor: 0,
+    }
+
+    this.startSensor = this.startSensor.bind(this)
+    this.checkStatus = this.checkStatus.bind(this)
   }
 
-  startStepCounter() {
-    console.log(SensorManager)
-    // SensorManager.startStepCounter(1000);
-    // DeviceEventEmitter.addListener('StepCounter', function (data) {
-    //   /**
-    //   * data.steps
-    //   **/
-    //   console.log(data)
-    // });
+  startSensor () {
+    console.log('checkStatus')
+    this.setState({
+      accelX: 0
+    })
+    DeviceEventEmitter.addListener('LightSensor', (data) => {
+      this.setState({
+        lightSensor: data.light
+      })
+    });
+    DeviceEventEmitter.addListener('StepCounter', (data) => {
+      this.setState({
+        step: this.state.step + 1
+      })
+    });
+    DeviceEventEmitter.addListener('StepCounter', (data) => {
+      this.setState({
+        step: this.state.step + 1,
+        status: 'walk/run'
+      })
+    });
+
+    DeviceEventEmitter.addListener('Accelerometer', (data) => {
+      this.setState({
+        accelX: data.x,
+        accelY: data.y,
+        accelZ: data.z,
+      })
+    });
+
+    DeviceEventEmitter.addListener('Gyroscope', (data) => {
+      this.setState({
+        gyroX: data.x,
+        gyroY: data.y,
+        gyroZ: data.z,
+      })
+    });
+
   }
+
+  checkStatus() {
+    //if banyak set state statusnya
+    console.log('checkStatus')
+    let accelXstatus = this.state.accelX < 1.5 && this.state.accelX > -1.5
+    let lightStatus = this.state.lightSensor < 10
+    if(lightStatus && accelXstatus){
+      this.setState({
+        status: 'rest/sleep'
+      })
+    } else if (accelXstatus) {
+      this.setState({
+        status: 'rest/sit'
+      })
+    }
+  }
+
+
 
   componentDidMount() {
-    SensorManager.startGyroscope(100);
-    DeviceEventEmitter.addListener('Gyroscope', function (data) {
-      console.log(data);
-    });
+    SensorManager.startStepCounter(100);
+    SensorManager.startLightSensor(100);
+    SensorManager.startAccelerometer(1000);
+    SensorManager.startGyroscope(1000);
+    this.timer = setTimeout(() => {
+     console.log('I do not leak!');
+    }, 5000);
   }
 
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
+        <Text style={{ fontSize: 25 }}>
+          Human Tracker
         </Text>
-        <Text style={styles.instructions}>
-          To get started, edit App.js
+        <Text style={styles.fontSize}>
+          Your Status: { this.state.status }
         </Text>
-        <Text style={styles.instructions}>
-          {instructions}
-        </Text>
-        <Button
-          onPress={this.startStepCounter}
-          title="Start step"
-          color="#841584"
-        />
+        <View>
+          <Text style={styles.fontSize}>
+            Accelerometer:
+          </Text>
+          <View style={{ flexDirection: 'row'}}>
+            <View style={{ width: 110, }}>
+              <Text style={styles.fontSize}>
+                X
+              </Text>
+            </View>
+            <View style={{ width: 110, }}>
+              <Text style={styles.fontSize}>
+                Y
+              </Text>
+            </View>
+            <View style={{ width: 110, }}>
+              <Text style={styles.fontSize}>
+                Z
+              </Text>
+            </View>
+          </View>
+          <View style={{ flexDirection: 'row'}}>
+            <View style={{ width: 110, }}>
+              <Text style={styles.fontSize}>
+                { this.state.accelX }
+              </Text>
+            </View>
+            <View style={{ width: 110, }}>
+              <Text style={styles.fontSize}>
+                { this.state.accelY }
+              </Text>
+            </View>
+            <View style={{ width: 110, }}>
+              <Text style={styles.fontSize}>
+                { this.state.accelZ }
+              </Text>
+            </View>
+          </View>
+        </View>
+
+        <View>
+          <Text style={styles.fontSize}>
+            Gyroscope:
+          </Text>
+          <View style={{ flexDirection: 'row'}}>
+            <View style={{ width: 110, }}>
+              <Text style={styles.fontSize}>
+                X
+              </Text>
+            </View>
+            <View style={{ width: 110, }}>
+              <Text style={styles.fontSize}>
+                Y
+              </Text>
+            </View>
+            <View style={{ width: 110, }}>
+              <Text style={styles.fontSize}>
+                Z
+              </Text>
+            </View>
+          </View>
+          <View style={{ flexDirection: 'row'}}>
+            <View style={{ width: 110, }}>
+              <Text style={styles.fontSize}>
+                { this.state.gyroX }
+              </Text>
+            </View>
+            <View style={{ width: 110, }}>
+              <Text style={styles.fontSize}>
+                { this.state.gyroY }
+              </Text>
+            </View>
+            <View style={{ width: 110, }}>
+              <Text style={styles.fontSize}>
+                { this.state.gyroZ }
+              </Text>
+            </View>
+          </View>
+        </View>
+
+        <View>
+          <Text style={styles.fontSize}>
+            Step:
+          </Text>
+          <View style={{ flexDirection: 'row'}}>
+            <View>
+              <Text style={styles.fontSize}>
+                { this.state.step }
+              </Text>
+            </View>
+          </View>
+        </View>
+
+        <View>
+          <Text style={styles.fontSize}>
+            LightSensor:
+          </Text>
+          <View style={{ flexDirection: 'row'}}>
+            <View>
+              <Text style={styles.fontSize}>
+                { this.state.lightSensor }
+              </Text>
+            </View>
+          </View>
+        </View>
+
+        <View>
+          <View style={{ marginTop: 10 }}>
+            <Button
+              onPress={this.startSensor}
+              title="Start Sensor"
+              color="#841584"
+            />
+          </View>
+          <View style={{ marginTop: 10 }}>
+            <Button
+              onPress={this.checkStatus}
+              title="Check Status"
+              color="#841584"
+            />
+          </View>
+        </View>
       </View>
     );
   }
@@ -71,18 +244,11 @@ export default class App extends Component<{}> {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
     backgroundColor: '#F5FCFF',
+    marginTop: 10,
+    marginLeft: 5,
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+  fontSize: {
+    fontSize: 20
+  }
 });
